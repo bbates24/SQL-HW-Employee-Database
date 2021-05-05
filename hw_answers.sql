413499247c30497445319e0e380dbf01a4aad2cc
brian bates <batesbj24@gmail.com>
2021-05-04 23:45:25 -0400

    added file

---
+++ hw_answers.sql
@@ -0,0 +1,48 @@
+-- Question 1
+Select emp_no, last_name, first_name,sex
+from employees
+Select salary from salaries;
+
+
+--Question 2
+Select first_name, last_name
+from employees;
+SELECT column_name hire_date;
+FROM employees
+WHERE condition
+GROUP BY hire_date "1986"
+ORDER BY hire_date;
+
+
+-- Question 3
+Select * from dept_manager;
+Select dept_name from departments;
+Select last_name, first_name from employees
+
+
+-- Question 4
+Select dept_name from departments;
+Select last_name, first_name, emp_no from employees;
+
+
+-- Question 5
+SELECT first_name,last_name, sex from employees;
+WHERE condition;
+GROUP BY column_name first_name "Hercules";
+ORDER BY last_name begin with "B"
+
+
+-- Question 6
+Select Distinct 'sales'dept_name from departments;
+Select emp_no,last_name, first_name from employees
+
+
+--Question 7
+Select Distinct 'sales', 'development' from departments;
+Select emp_no,last_name,first_name from employees
+
+
+-- Question 8
+SELECT MAX(last_name)
+FROM employees
+WHERE condition 'last_name' is same ;
